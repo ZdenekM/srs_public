ba41c9e7a4a96c4464c7f88c5d307dd8cd919396
ZdenekM <imaterna@fit.vutbr.cz>
2013-06-10 10:50:20 +0200

    Reduced rate of teleportation.

--- srs_user_tests/ros/scripts/move_robot_sim.py
+++ srs_user_tests/ros/scripts/move_robot_sim.py
@@ -164,7 +164,7 @@ def main():
       pub_s.publish(EmptyMsg())
       pub_s.publish(EmptyMsg())
       
-      r = rospy.Rate(2.0) # was 0.02
+      r = rospy.Rate(0.033) # once per 30s
       
       if not per:
       
